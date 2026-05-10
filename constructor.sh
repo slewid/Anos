609c57220c07f6501702ba69bec8c5991ece906d
lewid <lewie.dudding@gmail.com>
2026-05-10 12:11:31 +1200

    Cleaned zeroes.bin

--- constructor.sh
+++ constructor.sh
@@ -6,7 +6,6 @@ mkdir -p "temp"
 #compile asm
 nasm "code/kernel_entry.asm" -f elf -o "temp/kernel_entry.o"
 nasm -f bin "code/boot.asm" -o "temp/boot.bin"
-nasm -f bin "code/zeroes.asm" -o "temp/zeroes.bin"
 
 #compile c
 i386-elf-gcc -ffreestanding -m32 -g -c "code/kernel.c" -o "temp/kernel.o"
@@ -15,8 +14,9 @@ i386-elf-gcc -ffreestanding -m32 -g -c "code/kernel.c" -o "temp/kernel.o"
 i386-elf-ld -o "temp/full_kernel.bin" -Ttext 0x7e00 "temp/kernel_entry.o" "temp/kernel.o" --oformat binary -nostdlib
 
 #concatenate
-cat "temp/boot.bin" "temp/full_kernel.bin" > "temp/everything.bin"
-cat "temp/everything.bin" "temp/zeroes.bin" > "OS.bin"
+cat "temp/boot.bin" "temp/full_kernel.bin" > "OS.bin"
+
+truncate -s 1474560 OS.bin
 
 #run
 qemu-system-x86_64 -drive format=raw,file="OS.bin",index=0,if=floppy -m 128M
