9a26f5ada201492719e5e4a12da2d8b14c5c7719
lewid <lewie.dudding@gmail.com>
2026-05-12 20:51:42 +1200

    changed consructor

--- constructor.sh
+++ constructor.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 set -e
+clear
 
-mkdir -p "temp"
+echo "== Cleaning/build folder =="
+rm -rf "build os.img"
+mkdir -p build
 
-#compile asm
-nasm "code/kernel_entry.asm" -f elf -o "temp/kernel_entry.o"
-nasm -f bin "code/boot.asm" -o "temp/boot.bin"
+echo "== Assembling bootloader (sector 0) =="
+nasm -f bin "code/boot.asm" -o "build/boot.bin"
 
-#compile c
-i386-elf-gcc -ffreestanding -m32 -g -c "code/kernel.c" -o "temp/kernel.o"
+echo "== Assembling stage2 (sector 1) =="
+nasm -f bin "code/stage2.asm" -o "build/stage2.bin"
 
-#link
-i386-elf-ld -o "temp/full_kernel.bin" -Ttext 0x1000 "temp/kernel_entry.o" "temp/kernel.o" --oformat binary -nostdlib
+echo "== Assembling kernel entry =="
+nasm -f elf32 "code/kernel_entry.asm" -o "build/kernel_entry.o"
 
-#concatenate
-cat "temp/boot.bin" "temp/full_kernel.bin" > "OS.bin"
+echo "== Compiling C code =="
+i386-elf-gcc -ffreestanding -m32 -g -c "code/kernel.c" -o "build/kernel.o"
 
-truncate -s 1474560 OS.bin
+echo "== Linking kernel (entry + C kernel) =="
+i386-elf-gcc -m elf_i386 \
+    -Ttext 0x1000 \
+    "build/kernel_entry.o" "build/kernel.o" \
+    -o "build/kernel.bin" \
+    --oformat binary \
+    --nostdlib
 
-#run
-qemu-system-x86_64 -drive format=raw,file="OS.bin",index=0,if=floppy -m 128M
+echo "== Creating raw disk image (64MB) =="
+dd if=/dev/zero of=disk.img bs=1M count=64 status=none
+
+echo "== Writing bootloader (sector 0) =="
+dd if="build/boot.bin" of="disk.img" conv=notrunc status=none
+
+echo "== Writing stage2 (sector 1) ==""
+dd if="build/stage2.bin" of="disk.img" bs-1M seek=1 conv=notrunc status=none
+
+echo "== Writing kernel (sector 10) ==''
+dd if="build/stage2.bin" of="disk.img" bs-1M seek=10 conv=notrunc status=none
+
+qemu-system-i386 -drive format=raw,file=disk.img -m 128M
