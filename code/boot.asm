5b38706a858fc1e463e79a51eb6e329ac6fe63a6
lewid <lewie.dudding@gmail.com>
2026-05-12 21:28:54 +1200

    fixed stage 1 bootloader

--- code/boot.asm
+++ code/boot.asm
@@ -1,4 +1,5 @@
 [org 0x7c00]
+mov [BOOT_DRIVE], dl
 
 ; prints B to screen if correctly booted first stage
 mov ah, 0x0e
@@ -12,14 +13,25 @@ int 0x10
 mov al, 10
 int 0x10
 
+; stack
+xor ax, ax
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov sp, 0x7c00
+
 ; load sector from disk
+mov ax, 0x0000
+mov es, ax
 mov bx, 0x7e00
+
 mov ah, 0x02
 mov al, 1
 mov ch, 0
 mov cl, 2
 mov dh, 0
-mov dl, 0x80
+
+mov dl, [BOOT_DRIVE]
 int 0x13
 
 jc disk_error
@@ -33,6 +45,8 @@ disk_error:
     mov al, 'E'
     int 0x10
     jmp $
+
+BOOT_DRIVE: db 0
 
 times 510-($-$$) db 0
 dw 0xaa55
