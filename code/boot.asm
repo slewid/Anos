7a986934bdfd0f5c9bd995961c7c48751fd2b87f
lewid <lewie.dudding@gmail.com>
2026-05-12 20:52:02 +1200

    started 2 stage bootloader

--- code/boot.asm
+++ code/boot.asm
@@ -1,96 +1,38 @@
-[org 0x7c00]                        
-KERNEL_LOCATION equ 0x1000
-                                    
+[org 0x7c00]
 
-mov [BOOT_DISK], dl                 
+; prints B to screen if correctly booted first stage
+mov ah, 0x0e
+mov al, 'B'
+int 0x10
 
-                                    
-xor ax, ax                          
-mov es, ax
-mov ds, ax
-mov bp, 0x8000
-mov sp, bp
+; newline
+mov ah, 0x0e
+mov al, 13
+int 0x10
+mov al, 10
+int 0x10
 
-mov bx, KERNEL_LOCATION
-mov dh, 32
-
+; load sector from disk
+mov bx, 0x7e00
 mov ah, 0x02
-mov al, dh 
-mov ch, 0x00
-mov dh, 0x00
-mov cl, 0x02
-mov dl, [BOOT_DISK]
-int 0x13                
+mov al, 1
+mov ch, 0
+mov cl, 2
+mov dh, 0
+mov dl, 0x80
+int 0x13
+
 jc disk_error
 
-                                    
-mov ah, 0x0
-mov al, 0x3
-int 0x10                
+; jump to loaded sector
+jmp 0x0000:0x7e00
 
-
-CODE_SEG equ GDT_code - GDT_start
-DATA_SEG equ GDT_data - GDT_start
-
-cli
-lgdt [GDT_descriptor]
-mov eax, cr0
-or eax, 1
-mov cr0, eax
-jmp CODE_SEG:start_protected_mode
-
-jmp $
-                                    
+; print E if sector not loaded
 disk_error:
     mov ah, 0x0e
-    mov al, 'e'
+    mov al, 'E'
     int 0x10
     jmp $
-BOOT_DISK: db 0
 
-GDT_start:
-    GDT_null:
-        dd 0x0
-        dd 0x0
-
-    GDT_code:
-        dw 0xffff
-        dw 0x0
-        db 0x0
-        db 0b10011010
-        db 0b11001111
-        db 0x0
-
-    GDT_data:
-        dw 0xffff
-        dw 0x0
-        db 0x0
-        db 0b10010010
-        db 0b11001111
-        db 0x0
-
-GDT_end:
-
-GDT_descriptor:
-    dw GDT_end - GDT_start - 1
-    dd GDT_start
-
-
-[bits 32]
-start_protected_mode:
-    mov ax, DATA_SEG
-	mov ds, ax
-	mov ss, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	
-	mov ebp, 0x90000		
-	mov esp, ebp
-
-    jmp KERNEL_LOCATION
-
-                                     
- 
-times 510-($-$$) db 0              
+times 510-($-$$) db 0
 dw 0xaa55
